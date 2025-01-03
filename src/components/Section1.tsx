daffb199749b5b5a4ab8179e9b4a3eb8251c853f
Arpit <omagrahari55@gmail.com>
2025-01-03 22:22:11 +0530

    section1 ui changes and also added the aos for animation

--- src/components/Section1.tsx
+++ src/components/Section1.tsx
@@ -3,25 +3,33 @@ import Navbar from './Navbar'
 import Image from 'next/image';
 import PortfolioImg from "@/assets/portfolioImg.png"
 import { TypeAnimation } from 'react-type-animation';
+import { useEffect } from 'react';
+import AOS from 'aos' ;
+import 'aos/dist/aos.css';
 const Section1 = () => {
+
+    useEffect(() => {
+        AOS.init();
+    }, [])
   return (
     <>
     <Navbar/>
     <div>
-        <div className='flex items-center justify-center pt-[5rem] cursor-default  '>
-            <h1 className='Francisco-font text-[#A5DAF1] w-[30%] text-[10rem] text-end'>Hi<span className=''> I'm</span></h1>
-            <div className='w-[20%] border-gray-500 px-[2rem] pt-[2rem] bg-[#1D1D1D] rounded-[1rem]
-             mx-[5rem] rotate-[6deg] relative transition-transform hover:top-[-15px] cursor-pointer'>
-                <Image className='w-full' src={PortfolioImg} alt='Hehe'  />
+        <div className='flex items-center justify-center pt-[5rem] cursor-default overflow-hidden  '>
+            <h1 data-aos="fade-right" data-aos-duration="3000" data-aos-delay="500" className='Francisco-font text-[#A5DAF1] w-[30%] text-[10rem] text-end'>Hi<span className=''> I'm</span></h1>
+            <div data-aos="fade-down" data-aos-duration="3000" data-aos-delay="1000" className='w-[20%]  mx-[5rem]' >
+                <div  className='w-full border-gray-500 px-[2rem] pt-[2rem] bg-[#1D1D1D] rounded-[1rem]  rotate-[8deg]  transition-transform hover:top-[-15px] cursor-pointer relative object-cover'>
+                    <Image className='w-full' src={PortfolioImg} alt='Hehe'  />
+                </div>
             </div>
-            <div className='min-w-[30%]'>
+            <div data-aos="fade-left" data-aos-duration="3000" data-aos-delay="500"  className='min-w-[30%]'>
             <TypeAnimation
                 sequence={[
                     'Arpit Agrahari',
                     1500, 
                     'Full-Stack Dev',
                     1500,
-                    'Web Weave ',
+                    'Web Weaver',
                     1500,
                     'Code Creator',
                     1500
