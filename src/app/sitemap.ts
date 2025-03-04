9b46a305e99f4db88f081f93c8ce6157d4e14360
Arpit <omagrahari55@gmail.com>
2025-03-05 01:47:30 +0530

    seo and contact page done

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,14 +1,14 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { MetadataRoute } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.setHeader("Content-Type", "text/xml");
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-      <loc>https://arpitdev.vercel.app/</loc>
-      <lastmod>${new Date().toISOString()}</lastmod>
-      <priority>1.0</priority>
-    </url>
-  </urlset>`;
-  res.status(200).send(sitemap);
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://arpitdev.vercel.app/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://arpitdev.vercel.app/contact",
+      lastModified: new Date(),
+    },
+  ];
 }
